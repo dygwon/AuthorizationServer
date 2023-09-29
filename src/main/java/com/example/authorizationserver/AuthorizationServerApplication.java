d54e182196c9dfe47b3da6b6523518eb0ca4f07b
Daniel Gwon <dygwon@gmail.com>
2023-09-29 19:15:30 -0400

    Add development data.

--- src/main/java/com/example/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/com/example/authorizationserver/AuthorizationServerApplication.java
@@ -1,13 +1,26 @@
 package com.example.authorizationserver;
 
+import com.example.authorizationserver.users.TacoUser;
+import com.example.authorizationserver.users.TacoUserRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner dataLoader(TacoUserRepository userRepository, PasswordEncoder encoder) {
+        return args -> {
+            userRepository.save(new TacoUser("habuma", encoder.encode("password"), "ROLE_ADMIN"));
+            userRepository.save(new TacoUser("tacochef", encoder.encode("password"), "ROLE_ADMIN"));
+        };
     }
 
 }
